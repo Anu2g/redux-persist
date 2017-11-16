4ca4ea0d0a00b0969fd41a7b528879c25bfda071
Anu Bhaskar <anu@hurdlr.com>
2017-11-15 20:31:02 -0500

    generic API

--- src/createPersistoid.js
+++ src/createPersistoid.js
@@ -8,6 +8,7 @@ export default function createPersistoid(config: PersistConfig): Persistoid {
   // defaults
   const blacklist: ?Array<string> = config.blacklist || null
   const whitelist: ?Array<string> = config.whitelist || null
+  const largeObjects: ?Array<string> = config.largeObjects || null
   const transforms = config.transforms || []
   const throttle = config.throttle || 0
   const storageKey = `${config.keyPrefix !== undefined
@@ -30,7 +31,7 @@ export default function createPersistoid(config: PersistConfig): Persistoid {
       if (!passWhitelistBlacklist(key)) return // is keyspace ignored? noop
       if (lastState[key] === state[key]) return // value unchanged? noop
       if (keysToProcess.indexOf(key) !== -1) return // is key already queued? noop
-      if (key === "entities" && !includeLargeObjects) return;
+      if (largeObjects.indexOf(key) && !includeLargeObjects) return;
       keysToProcess.push(key) // add key to queue
     })
 
